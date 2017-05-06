7cb72b8d519af4b7e9e262ad77e4bcaeb636067c
nagpaulo <nagpaulo@gmail.com>
2017-05-05 23:50:31 -0300

    Init Template

--- src/App.js
+++ src/App.js
@@ -1,65 +1,10 @@
 import React, { Component } from 'react';
-import $ from 'jquery';
 import './assets/css/bootstrap.min.css';
 import './assets/css/font-awesome.min.css';
 import './assets/css/base.min.css';
-import InputCustomizado from './components/InputCustomizado';
-import GroupButtonSearchComponent from './components/GroupButtonSearchComponent';
+import AutorBox from './model/Autor';
 
 class App extends Component {
-    constructor(){
-        super();
-        this.state = {lista : [],nome:'',email:'',senha:''};
-        this.enviaForm = this.enviaForm.bind(this);
-        this.setNome = this.setNome.bind(this);
-        this.setEmail = this.setEmail.bind(this);
-        this.setSenha = this.setSenha.bind(this);
-    }
-
-    enviaForm(event){
-        event.preventDefault();
-        $.ajax({
-            type: 'post',
-            url: 'http://localhost:8080/tethys/api/autores',
-            data: JSON.stringify({nome:this.state.nome, email:this.state.email, senha:this.state.senha}),
-            contentType: "application/json; charset=utf-8",
-            traditional: true,
-            success: function(resposta){
-                this.setState({lista:resposta})
-            }.bind(this),
-            error: function(resposta){
-                console.log("erro: "+resposta);
-            }
-        });
-    }
-
-    setNome(event){
-        this.setState({nome:event.target.value});
-    }
-
-    setEmail(event){
-        this.setState({email:event.target.value});
-    }
-
-    setSenha(event){
-        this.setState({senha:event.target.value});
-    }
-
-    componentDidMount(){
-        $.ajax({
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'text/plain'
-            },
-            async:false,
-            url:"http://localhost:8080/tethys/api/autores",
-            dataType: 'json',
-            success: function (resposta) {
-                this.setState({lista:resposta})
-            }.bind(this)
-        });
-    }
-
     render() {
         return (
             <div id="template">
@@ -113,46 +58,7 @@ class App extends Component {
                         <div className="panel panel-default">
                             <div className="panel-body">
                                 <div className="alert-container"></div>
-                                <form className="form" onSubmit={this.enviaForm} method="post">
-                                    <InputCustomizado div="col-md-9" label="Nome" type="text" name="nm_autor" id="nm_autor" value={this.state.nome} onChange={this.setNome}/>
-                                    <InputCustomizado div="col-md-6" label="Email" type="email" name="nm_email" id="nm_email" value={this.state.email} onChange={this.setEmail}/>
-                                    <InputCustomizado div="col-md-6" label="Senha" type="password" name="nm_senha" id="nm_senha" value={this.state.senha} onChange={this.setSenha}/>
-                                    <GroupButtonSearchComponent div="col-md-9" id="btSearch"/>
-                                </form>
-
-                                <div className="row">
-                                    <div className="col-lg-12">
-                                        <table className="table table-striped table-hover">
-                                            <thead>
-                                                <tr>
-                                                    <th width="25" className="check">
-                                                        <input type="checkbox" id="btCheckAll"/></th>
-                                                    <th className="text-left">Nome</th>
-                                                    <th className="text-left">Email</th>
-                                                    <th></th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                            {
-                                                this.state.lista.map(function (autor) {
-                                                    return (
-                                                      <tr key={autor.id}>
-                                                          <td><input type="checkbox" id="btCheckOne" value={autor.id}/></td>
-                                                          <td>{autor.nome}</td>
-                                                          <td>{autor.email}</td>
-                                                          <td>
-                                                              <button type="button" className="btn btn-default btn-xs" title="Editar" data-toggle="tooltip">
-                                                                  <span className="fa fa-pencil"></span>
-                                                              </button>
-                                                          </td>
-                                                      </tr>
-                                                    );
-                                                })
-                                            }
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
+                                <AutorBox/>
                             </div>
                         </div>
                     </div>
---
+++ src/model/Autor.js
@@ -0,0 +1,135 @@
+/**
+ * Created by Paulo Roberto on 05/05/2017.
+ */
+import React, { Component } from 'react';
+import $ from 'jquery';
+import InputCustomizado from '../components/InputCustomizado';
+import GroupButtonSearchComponent from '../components/GroupButtonSearchComponent';
+
+class FormularioAutor extends Component{
+    constructor(){
+        super();
+        this.state = {nome:'',email:'',senha:''};
+        this.enviaForm = this.enviaForm.bind(this);
+        this.setNome = this.setNome.bind(this);
+        this.setEmail = this.setEmail.bind(this);
+        this.setSenha = this.setSenha.bind(this);
+    }
+
+    enviaForm(event){
+        event.preventDefault();
+        $.ajax({
+            type: 'post',
+            url: 'http://localhost:8080/tethys/api/autores',
+            data: JSON.stringify({nome:this.state.nome, email:this.state.email, senha:this.state.senha}),
+            contentType: "application/json; charset=utf-8",
+            traditional: true,
+            success: function(resposta){
+                this.props.callbackAtualizaListagem(resposta);
+            }.bind(this),
+            error: function(resposta){
+                console.log("erro: "+resposta);
+            }
+        });
+    }
+
+    setNome(event){
+        this.setState({nome:event.target.value});
+    }
+
+    setEmail(event){
+        this.setState({email:event.target.value});
+    }
+
+    setSenha(event){
+        this.setState({senha:event.target.value});
+    }
+
+    render(){
+        return(
+            <form className="form" onSubmit={this.enviaForm} method="post">
+                <InputCustomizado div="col-md-9" label="Nome" type="text" name="nm_autor" id="nm_autor" value={this.state.nome} onChange={this.setNome}/>
+                <InputCustomizado div="col-md-6" label="Email" type="email" name="nm_email" id="nm_email" value={this.state.email} onChange={this.setEmail}/>
+                <InputCustomizado div="col-md-6" label="Senha" type="password" name="nm_senha" id="nm_senha" value={this.state.senha} onChange={this.setSenha}/>
+                <GroupButtonSearchComponent div="col-md-9" id="btSearch"/>
+            </form>
+        );
+    }
+
+}
+class TabelaAutores extends Component {
+
+    render(){
+        return(
+            <div className="row">
+                <div className="col-lg-12">
+                    <table className="table table-striped table-hover">
+                        <thead>
+                        <tr>
+                            <th width="25" className="check">
+                                <input type="checkbox" id="btCheckAll"/></th>
+                            <th className="text-left">Nome</th>
+                            <th className="text-left">Email</th>
+                            <th></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            this.props.lista.map(function (autor) {
+                                return (
+                                    <tr key={autor.id}>
+                                        <td><input type="checkbox" id="btCheckOne" value={autor.id}/></td>
+                                        <td>{autor.nome}</td>
+                                        <td>{autor.email}</td>
+                                        <td>
+                                            <button type="button" className="btn btn-default btn-xs" title="Editar" data-toggle="tooltip">
+                                                <span className="fa fa-pencil"></span>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                );
+                            })
+                        }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default class AutorBox extends Component{
+    constructor(){
+        super();
+        this.state = {lista : []};
+        this.atualizaListagem = this.atualizaListagem.bind(this);
+    }
+
+    componentDidMount(){
+        $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'text/plain'
+            },
+            async:false,
+            url:"http://localhost:8080/tethys/api/autores",
+            dataType: 'json',
+            success: function (resposta) {
+                this.setState({lista:resposta})
+            }.bind(this)
+        });
+    }
+
+    atualizaListagem(novaLista){
+        this.setState({lista:novaLista});
+    }
+
+    render(){
+        return(
+            <div id="autorBox">
+                <FormularioAutor callbackAtualizaListagem={this.atualizaListagem}/>
+                <TabelaAutores lista={this.state.lista}/>
+            </div>
+        );
+    }
+}
